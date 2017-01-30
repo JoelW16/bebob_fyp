2ab7017dc35c27576adc7d4b32fca0c7c2059138
joelw16 <joelw16@gmail.com>
2017-01-30 15:08:54 +0000

    Tracks ball to zones: Center: Top, Bottom, Left, Right Top: Left, Right Bottom: left, Right
    TODO:
    Implement publisher to telemetry node.
    Write a telemetry node that subscribes to track_ball, and publishes telemetry data to the camera (for testing) and flight nodes.

--- src/bebop_fyp/scripts/track_ball.py
+++ src/bebop_fyp/scripts/track_ball.py
@@ -10,9 +10,13 @@ import numpy as np
 import argparse
 
 class image_converter:
-
+        #cv2.line(cv_image, (0, height/3), (width, height/3), (0, 255, 0), 1)
+        #cv2.line(cv_image, (0, height -height/3), (width,height- height/3), (0, 255, 0), 1)
+        #cv2.line(cv_image, (width/3, 0), (width/3, height), (255, 255, 255), 1)
+        #cv2.line(cv_image, (width - width/3, 0), (width -width/3, height), (255, 255, 255), 1)
     def __init__(self):
 
+        self.first = True
         self.ap = argparse.ArgumentParser()
         self.ap.add_argument("-b", "--buffer", type=int, default=64)
         args = vars(self.ap.parse_args())
@@ -35,11 +39,36 @@ class image_converter:
         def nothing(x):
             pass
 
+        height = np.size(cv_image, 0)
+        width = np.size(cv_image, 1)
+        center_center_boundary = [(width/3, height/3), ((width - width/3), (height - height/3))]
+        left_center_boundary = [(0, height/3), (width/3, height - height/3)]
+        right_center_boundary = [(width/3, height/3), (width, height - height/3)]
+        top_center_boundary = [(width/3, 0), (width-(width/3), height/3)]
+        bottom_center_boundary = [(width/3, height - (height/3)), (width - (width / 3), height)]
+
+
+        if self.first:
+            print height, width
+            print center_center_boundary
+            self.first = False
+
+
+        #cv2.line(cv_image, (width/2, 0), (width/2, height), (255, 255, 255), 1)
+        #cv2.line(cv_image, (0, height/2), (width, height/2), (255, 255, 255), 1)
+
+        cv2.rectangle(cv_image, left_center_boundary[0], left_center_boundary[1],  (255, 0, 0), 1)
+        cv2.rectangle(cv_image, right_center_boundary[0], right_center_boundary[1], (255, 0, 0), 1)
+        cv2.rectangle(cv_image, top_center_boundary[0], top_center_boundary[1], (255, 0, 0), 1)
+        cv2.rectangle(cv_image, bottom_center_boundary[0], bottom_center_boundary[1], (255, 0, 0), 1)
+        cv2.rectangle(cv_image, center_center_boundary[0], center_center_boundary[1], (0, 255, 0), 1)
+
         hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 
         mask = cv2.inRange(hsv, self.lower_blue, self.upper_blue)
         mask = cv2.erode(mask, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=5)
+        font = cv2.FONT_HERSHEY_PLAIN
 
 
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
@@ -48,26 +77,40 @@ class image_converter:
 
         if len(cnts) > 0:
 
-
-
             c = max(cnts, key=cv2.contourArea)
             ((x, y), radius) = cv2.minEnclosingCircle(c)
             M = cv2.moments(c)
-            center= (int(M["m10"] / M["m00"]),int( M["m01"] / M["m00"] ))
+            center = (int(M["m10"] / M["m00"]),int( M["m01"] / M["m00"]))
 
-            if(center > (450,0)):
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(cv_image, 'Right', (520, 100), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(cv_image, center.__str__(), (10, 65), font, 2, (0, 255, 0), 2, cv2.LINE_AA)
+            cv2.putText(cv_image, radius.__str__(), (10, 95), font, 2, (0, 255, 0), 2, cv2.LINE_AA)
+
+            if all(i > j for i, j in zip(center,center_center_boundary[0])) and all(i < j for i, j in zip(center,center_center_boundary[1])):
+                cv2.putText(cv_image, 'Center', (10, 30), font, 2, (0, 255, 0), 2, cv2.LINE_AA)
+            elif all(i > j for i, j in zip(center,top_center_boundary[0])) and all(i < j for i, j in zip(center,top_center_boundary[1])):
+                cv2.putText(cv_image, 'Top Center', (10, 30), font, 2, (255, 0, 0), 2, cv2.LINE_AA)
+            elif all(i > j for i, j in zip(center, bottom_center_boundary[0])) and all(i < j for i, j in zip(center, bottom_center_boundary[1])):
+                cv2.putText(cv_image, 'Bottom Center', (10, 30), font, 2, (255, 0, 0), 2, cv2.LINE_AA)
+            elif all(i > j for i, j in zip(center, left_center_boundary[0])) and all(i < j for i, j in zip(center, left_center_boundary[1])):
+                cv2.putText(cv_image, 'Left Center', (10, 30), font, 2, (255, 0, 0), 2, cv2.LINE_AA)
+            elif all(i > j for i, j in zip(center, right_center_boundary[0])) and all(i < j for i, j in zip(center, right_center_boundary[1])):
+                cv2.putText(cv_image, 'Right Center', (10, 30), font, 2, (255, 0, 0), 2, cv2.LINE_AA)
+            elif all(i > j for i, j in zip(center, (width/2, 0))):
+                if all(i > j for i, j in zip(center, (0, height/2))):
+                    cv2.putText(cv_image, 'Right Bottom', (10, 30), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
+                else:
+                    cv2.putText(cv_image, 'Right Top', (10, 30), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
             else:
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(cv_image, 'Left', (10, 100), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+                if all(i > j for i, j in zip(center, (0, height/2))) :
+                    cv2.putText(cv_image, 'Left Bottom', (10, 30), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
+                else:
+                    cv2.putText(cv_image, 'Left Top', (10, 30), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
 
             if(radius > 10):
                 cv2.circle(cv_image, (int(x), int(y)), int(radius), (0,255,255), 2)
                 cv2.circle(cv_image, center, 5, (0, 0, 255), -1)
         else:
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(cv_image, 'No Ball', (10, 100), font, 4, (0, 0, 255), 2, cv2.LINE_AA)
+            cv2.putText(cv_image, 'No Ball', (10, 30), font, 2, (0, 0, 255), 2, cv2.LINE_AA)
 
         #res = cv2.bitwise_and(cv_image, cv_image, mask=mask)
 
