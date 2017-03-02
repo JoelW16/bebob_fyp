fe15176fbbee63bb191e5bd12d2a45cc6fec14b7
joelw16 <joelw16@gmail.com>
2017-03-02 19:34:25 +0000

    Myo intergration and API setup

--- src/bebop_fyp/scripts/api_drone.py
+++ src/bebop_fyp/scripts/api_drone.py
@@ -2,6 +2,7 @@
 import rospy
 import sys
 import requests
+import atexit
 from bebop_msgs.msg import CommonCommonStateBatteryStateChanged as b
 from bebop_msgs.msg import Ardrone3PilotingStatePositionChanged as gps
 
@@ -10,11 +11,12 @@ class server:
         self.battery = 0
         self.batt_sub = rospy.Subscriber("/bebop/states/common/CommonState/BatteryStateChanged", b, self.callback)
         self.gps_sub = rospy.Subscriber("/bebop/states/ardrone3/PilotingState/PositionChanged", gps, self.sendGPS)
+        self.start()
 
     def callback(self, data):
         print (data.percent)
         self.battery = data.percent
-        requests.put('http://52.56.154.153:3000/api/droneUpdateStatus/58ab72d5ff63d537a10e8a2c',
+        requests.put('http://52.56.154.153:3000/api/droneUpdateStatus/58b82b7d2cc965257c433dea',
                      data={'battery': self.battery})
 
     def sendGPS(self, data):
@@ -22,18 +24,26 @@ class server:
         print ("long" + str(data.longitude))
         print ("alt" + str(data.altitude))
 
-    def shutdown(self):
-        requests.put('http://52.56.154.153:3000/api/droneUpdateStatus/58ab72d5ff63d537a10e8a2c',
-                     data={'battery': 0})
+    def start(self):
+        print("Drone connect")
+        requests.put('http://52.56.154.153:3000/api/droneUpdateStatus/58b82b7d2cc965257c433dea',
+                     data={'battery' : self.battery, 'connected': "true"})
+
+def exit():
+    print("Drone disconnect")
+    requests.put('http://52.56.154.153:3000/api/droneUpdateStatus/58b82b7d2cc965257c433dea',
+                 data={'battery' : 0, 'connected': "false"})
 
 def main(args):
-    rospy.init_node('api')
+    atexit.register(exit)
+    rospy.init_node('api_drone')
     api = server()
     try:
         rospy.spin()
     except KeyboardInterrupt:
-        api.shutdown()
         rospy.signal_shutdown('Bye')
+
+
 
 if __name__ == '__main__':
     main(sys.argv)
---
+++ src/bebop_fyp/scripts/api_myo.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import rospy
+import sys
+import requests
+import numpy as np
+import atexit
+from ros_myo.msg import MyoPose
+
+
+class server:
+    def __init__(self):
+        self.pose = 0
+        self.pose_sub = rospy.Subscriber("/myo_raw/myo_gest", MyoPose, self.callback)
+        self.start();
+
+    def callback(self, data):
+        self.pose = np.int_(data.pose)
+        requests.put('http://52.56.154.153:3000/api/myoUpdateStatus/58b829c6280587252ed0bec8', data={'pose': self.pose})
+
+    def sendGPS(self, data):
+        print ("lat" + str(data.latitude))
+        print ("long" + str(data.longitude))
+        print ("alt" + str(data.altitude))
+
+    def start(self):
+        print("Myo connect")
+        requests.put('http://52.56.154.153:3000/api/myoUpdateStatus/58b829c6280587252ed0bec8',
+                     data={'connected': "true"})
+
+
+def main(args):
+    atexit.register(exit)
+    rospy.init_node('api_myo')
+    api = server()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        rospy.signal_shutdown('Bye')
+
+def exit():
+    print("Myo disconnect")
+    requests.put('http://52.56.154.153:3000/api/myoUpdateStatus/58b829c6280587252ed0bec8',
+                 data={'connected': "false"})
+
+if __name__ == '__main__':
+    main(sys.argv)
